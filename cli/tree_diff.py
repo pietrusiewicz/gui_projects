db23a46bc93f9caec45c0fa1cdbbba528ec83102
Kacper Pietrusiewicz <p4zdaninho.cr@gmail.com>
2022-05-22 13:39:04 +0200

    delete cli directory

--- cli/tree_diff.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.chdir('/')
-def tree_in_tree():
-    #{f:os.listdir(f) for f in os.listdir()}
-    return { f:os.listdir(f) for f in [dr for dr in os.listdir() if '.' not in dr[1:]] }
-        #return os.listdir(f))
-print(tree_in_tree())
