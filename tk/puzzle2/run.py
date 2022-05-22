cd8a3b2f1105bbccf2a54d4606a5a1331831f2a5
Kacper Pietrusiewicz <p4zdaninho.cr@gmail.com>
2022-05-22 13:18:45 +0200

    documentating the puzzles minigame

--- tk/puzzle2/run.py
+++ tk/puzzle2/run.py
@@ -2,89 +2,131 @@ from tkinter import Tk, Button, Label, PhotoImage
 from random import shuffle
 from PIL import Image, ImageTk
 import os
-import sys
+from sys import argv
 
 class Puzzle(Tk):
-    def __init__(self, img='linus.png', dimensions='2x2'):
+    def __init__(self, img='', dimensions='2x2'):
         Tk.__init__(self)
-        self.img = img
+        #print(argv)
+        self.img = argv[1] if len(argv) > 1 else ''
+        #print(self.img)
         self.dimensions = list(map(int,dimensions.split('x')))
         self.randomize_puzzles()
         self.moves = 1
         self.selected = []
         self.show_puzzles()
 
+    # prepare elements of puzzles
     def randomize_puzzles(self):  # {{{
+        # number of elements in row/column
         w,h = self.dimensions
-        #self.correct_puzzles = [[Button(self) for x in range(w)] for y in range(h)]
-        self.correct_puzzles = [[(x,y) for x in range(w)] for y in range(h)]
-        
-        #self.imgs = self.get_imgs()
-        #self.correct_puzzles = list(zip(self.correct_puzzles, imgs))
 
+        # create 2D array where [row[column]]
+        self.correct_puzzles = [[(x,y) for x in range(w)] for y in range(h)]
+
+        # convert puzzles to 1D array
         self.puzzles = []
         for tup in self.correct_puzzles:
             self.puzzles += tup
+
+        # shuffle puzzles in 1D array
         shuffle(self.puzzles)
+
+        # convert 1D array to 2D array drawing [row[column]]
         self.puzzles = [self.puzzles[y*w:(y+1)*w] for y in range(h)]
         # }}}
 
-    def show_puzzles(self,koniec=0): # {{{
+    # display puzzles
+    def show_puzzles(self): # {{{
+        # number of elements in row/column
         w,h = self.dimensions
-        #wycinki = [[crop_img(x,y) for x in range(len(self.correct_puzzles[y]))] for y in range(length_rows)]
-        #for y, objs in enumerate(self.puzzles):
+
+        # for loop [ROW[column]]
         for y in range(h):
+            # for loop [row[COLUMN]]
             for x in range(w):
+                # variables for random coords in 2D array
+                # to select these puzzles
                 tx, ty = self.puzzles[y][x]
-                wycinek = self.get_crop(tx, ty)
-                img2 = ImageTk.PhotoImage(wycinek)
-                b = Button(self, image=img2)
-                b.image=img2
+
+                # declare single puzzle
+                # as Button() object to edit
+                b = Button(self, text=f"{tx},{ty}")
                 b.grid(row=y, column=x)
-                if koniec:
-                    b["state"]="disabled"
-                    continue
-                #tup, img = tup_img
-                #wy = ImageTk.PhotoImage(img.crop((x*dx,y*dy,(x+1)*dx,(y+1)*dy)))
-                #wycinek = pic.crop((tx*dx,ty*dy,(tx+1)*dx,(ty+1)*dy))
+
+                # when image is entered
+                if self.img !='': # {{{
+
+                    # crop entered image
+                    # by variables declared (50 line)
+                    wycinek = self.get_crop(tx, ty)
+
+                    # set cropped image
+                    # to needed class 
+                    img = ImageTk.PhotoImage(wycinek)
+                    # and needed key
+                    b.image=img # }}}
+
+                # what command is executed after click this element
                 b["command"] = lambda x=x,y=y: self.swap_puzzles([x,y])
-                if self.selected != []:
-                    if self.selected == [x,y]:
-                        b["state"]="disabled"
+
+                # this is clicked element
+                if self.moves % 2 and self.selected == [x,y]:
+                    b["state"]="disabled"  # }}}
 
     def swap_puzzles(self, xy): #{{{
+        # first clicked element
         x,y = xy
-        if self.moves % 2==0:
+
+        # two elements are clicked
+        if not (self.moves % 2):
+            
+            # second clicked element
             x1,y1 = self.selected
+
+            # replace two elements
             self.puzzles[y1][x1], self.puzzles[y][x]= self.puzzles[y][x], self.puzzles[y1][x1]
+
+            # clear coordinates of elements in self.selected
             self.selected = []
+
+
+            # puzzles are placed
             if self.puzzles == self.correct_puzzles:
                 self.win_screen()
+
+        # one element is clicked
         else:
             self.selected = [x,y]
 
+        # refresh puzzles table
         self.show_puzzles()
+
+        # incrementing number of moves
         self.moves += 1 # }}}
 
     def win_screen(self): # {{{
         w,h = self.dimensions
+        Label(self, text="koniec gry").grid(row=h-1, rowspan=h, columnspan=w)
+        b = Button(self, text="play again")
+        b["command"] = lambda: [b.grid_forget(), b.forget(),self.randomize_puzzles(), self.show_puzzles()]
+        b.grid(row=h, rowspan=h, columnspan=w) # }}}
+        """
+        print("kowno")
         for y in range(h):
             for x in range(w):
-                #tx, ty = self.puzzles[y][x]
-                wycinek = self.get_crop(x, y)
-                img2 = ImageTk.PhotoImage(wycinek)
-                b = Button(self, image=img2)
-                b.image=img2
-                b["state"]="disabled"
+                b = Button(self, text=f"{x},{y}")
+                if self.img != '':
+                    wycinek = self.get_crop(x, y)
+                    img = ImageTk.PhotoImage(wycinek)
+                    b.image=img
+                b.state="disabled"
                 b.grid(row=y, column=x)
-        #self.show_puzzles(1)
         #s = self.size()
-        l = Label(self, text="koniec gry")
-        l.grid(row=0, rowspan=h, columnspan=w)
         b = Button(self, text="play again")
         b["command"] = lambda: [b.grid_forget(), l.forget(),self.randomize_puzzles(), self.show_puzzles()]
         b.grid(rowspan=h, columnspan=w) # }}}
-
+        """
     def get_crop(self,tx,ty):
         pic = Image.open(self.img)
         s = pic.size
