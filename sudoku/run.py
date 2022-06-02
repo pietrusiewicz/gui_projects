3e35528ad6e1443ab2111b337bd82233475824cb
pazdaninho <p4zdaninho.cr@gmail.com>
2022-06-02 10:54:41 +0200

    checks values after insertion

--- sudoku/run.py
+++ sudoku/run.py
@@ -1,10 +1,14 @@
 from tkinter import Tk, Button, Text
+from threading import Thread
+import time
 
 class Sudoku(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.squares = [[Button(self, width=0, height=0, command=lambda x=x,y=y: self.click(x,y)) for x in range(9)] for y in range(9)]
         self.place_squares()
+        t1 = Thread(target=self.work)
+        t1.start()
 
     def place_squares(self):
         for y in range(9):
@@ -12,8 +16,18 @@ class Sudoku(Tk):
                 self.squares[y][x].grid(row=y, column=x)
 
     def click(self, x,y):
-        Text(self, width=2, height=1,bd=4).grid(row=y, column=x)
+        self.squares[y][x] = Text(self, width=2, height=1,bd=4)
+        self.squares[y][x].grid(row=y, column=x)
 
+    def work(self):
+        while True:
+            time.sleep(1)
+            for y in range(9):
+                for x in range(9):
+                    try:
+                        print(self.squares[y][x].get('1.0','end')[:1])
+                    except AttributeError:
+                        pass
 if __name__ == '__main__':
     s = Sudoku()
     s.mainloop()
