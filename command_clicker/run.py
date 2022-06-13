f30829a9c94672598358fae1c0bb553ec5cdc5b6
pazdaninho <p4zdaninho.cr@gmail.com>
2022-06-13 08:38:04 +0200

    commenting of command_clicker

--- command_clicker/run.py
+++ command_clicker/run.py
@@ -7,40 +7,56 @@ from os import system
 class Program(Tk):
     def __init__(self):
         Tk.__init__(self)
+
         # own command if given, default poweroffs
         self.command = ''.join(argv[1:]) if len(argv) > 1 else "systemctl poweroff"
+
         self.dest_time = time.time()+200
         self.display_button()
+        t1 = Thread(target=self.work)
+        t1.start()
 
-    def display_button(self):
+    # display button with time to end
+    def display_button(self): # {{{
         # declare a button
         self.b = Button(self, command=self.click, bd=3, width=10)
         self.b.grid(row=0, column=0)
 
         # place text in self.b (16 line)
-        self.b["text"] = f"{self.diff()}"
-        t1 = Thread(target=self.work)
-        t1.start()
+        self.b["text"] = f"{self.diff()}" # }}}
 
-    def work(self):
-        time.sleep(0.6)
-        # when difference is equal or less zero
-        if self.diff() <= 0:
-            # execution of commsnd
-            system(self.command)
-            self.end_sequence()
-        self.display_button()
+    # work while tkinter is working
+    def work(self): # {{{
+        while True:
+            time.sleep(0.6)
 
-    def click(self):
-        self.dest_time += 4
+            # when difference is equal or less zero
+            if self.diff() <= 0:
+
+                # execution of commsnd
+                system(self.command)
+
+                # exit from application
+                self.end_sequence()
+                #self.dest_time = time.time()+200
+            self.display_button() # }}}
+
+    # click the button
+    def click(self): # {{{
+        "adding time to execute"
+        self.dest_time += 4 # }}}
 
     # time to execute command
-    def diff(self):
-        return int(self.dest_time-time.time())
+    def diff(self): # {{{
+        "difference of times"
+        return int(self.dest_time-time.time()) # }}}
 
-    def end_sequence(self):
+    # end sequence
+    def end_sequence(self): # {{{
+        "destroy program, exit"
         self.destroy()
-        exit()
+        exit() # }}}
+
 if __name__ == '__main__':
     p = Program()
     p.mainloop()
