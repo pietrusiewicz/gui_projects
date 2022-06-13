b39b975ca22bec8c7e54f21380893c7e3394719d
pazdaninho <p4zdaninho.cr@gmail.com>
2022-06-13 08:27:44 +0200

    commenting clock bit program

--- clock_bit/run.py
+++ clock_bit/run.py
@@ -7,33 +7,43 @@ class App(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.display_bit_clock()
+        t1 = Thread(target=self.work)
+        t1.start()
 
-
+    # display digit clock
     def display_bit_clock(self):
+        "display one or zero"
         # get current time
         s = time.strftime("%H:%M:%S")
         Label(self, text=s).grid(row=0, column=0, columnspan=6)
         # get []
         s = [list(map(lambda x: bin(int(x.split()[0]))[2:], e.split()[0])) for e in s.split(":")]
-        l = []
+
+        # convert s to array
+        l = [] # {{{
         for arr in s:
-            l += arr
+            l += arr # }}}
+
+        # place Labels with spaces
         [[Label(self, text=" ",fg="#ececec", bg="#d2d2d2", bd=8).grid(row=y+1, column=x) for x in range(6)] for y in range(4)]
-        # ['1','100','1100', '111']
-        for i,bins in enumerate(l):
+
+        # place one or zero value
+        for i,bins in enumerate(l): # {{{
             # ['1','100']
             for j,num in enumerate(bins):
                 if int(num): 
                     l = Label(self, text=num, bd=8, fg="white", bg="black")
                 else:
                     l = Label(self, text=num, bd=8, fg="black", bg="white")
-                l.grid(row=j+1, column=i)
-        t1 = Thread(target=self.work)
-        t1.start()
+                l.grid(row=j+1, column=i) # }}}
 
+
+    # execute sleep command in tkinter
     def work(self):
-        time.sleep(1)
-        self.display_bit_clock()
+        "example of sleep command while tkinter program"
+        while True:
+            time.sleep(1)
+            self.display_bit_clock()
 
 if __name__ == '__main__':
     a = App()
