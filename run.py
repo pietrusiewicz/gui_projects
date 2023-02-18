9b69831ebb14fa279b411b2c9bdf0c3f91613564
Kacper Pietrusiewicz <p4zdaninho.cr@gmail.com>
2023-02-19 00:26:18 +0100

    add few new options apart mark item

--- run.py
+++ run.py
@@ -1,9 +1,23 @@
 class Todolist:
     def __init__(self):
         self.items = {}
-        self.menu()
 
     def menu(self):
         print("Items:")
-        for item, done in self.items():
-            print(f"\n\t{item}: {done}")
+        for item, done in self.items.items():
+            print(f"\t'{item}': {done}")
+        select = int(input("1) Add Item 2) Delete Item 3) Mark Item *) Exit\n"))
+        if select == 1:
+            self.items[input("Name item: ")] = False
+        elif select == 2:
+            n = int(input("number of item: "))
+            del self.items[list(self.items[n])]
+        elif select == 3:
+            n = int(input("number of item: "))
+            self.items[list(self.items[n])] = not bool(self.items[list(self.items[n])])
+        self.menu()
+
+if __name__ == "__main__":
+    t = Todolist()
+    t.menu()
+    
