c690d0ee0427e7b1d004a87ffbed3641d1bf96d6
Kacper Pietrusiewicz <p4zdaninho.cr@gmail.com>
2023-02-21 15:53:50 +0100

    repair todolist

--- run.py
+++ run.py
@@ -4,18 +4,28 @@ class Todolist:
 
     def menu(self):
         print("Items:")
-        for item, done in self.items.items():
-            print(f"\t'{item}': {done}")
+        for i, items in enumerate(self.items.items()):
+            item, done = items
+            print(f"\t'{i}) {item}': {done}")
         select = int(input("1) Add Item 2) Delete Item 3) Mark Item *) Exit\n"))
         if select == 1:
-            self.items[input("Name item: ")] = False
+            self.items[self.add_item()] = False
         elif select == 2:
             n = int(input("number of item: "))
-            del self.items[list(self.items[n])]
+            key = list(self.items)[n]
+            del self.items[key]
         elif select == 3:
             n = int(input("number of item: "))
-            self.items[list(self.items[n])] = not bool(self.items[list(self.items[n])])
+            key = list(self.items)[n]
+            if self.items[key]:
+                self.items[key] = False
+            else:
+                self.items[key] = True
         self.menu()
+
+    def add_item():
+        return input("Name item: ")
+
 
 if __name__ == "__main__":
     t = Todolist()
