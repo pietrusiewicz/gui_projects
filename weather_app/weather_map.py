945aa7e4751552ba97870ff9cd508930aa157da9
Kacper Pietrusiewicz <p4zdaninho.cr@gmail.com>
2023-03-06 16:35:06 +0100

    change name of file

--- weather_app/weather_map.py
+++ weather_app/weather_map.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 import json
 import math
+import weather_data
+
 
 class Weather_map(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.cities = json.load(open("miasta_pl.json"))
+
 
     def display_button(self, city):
         x,y = float(city['lon']), 60-float(city['lat'])
@@ -17,10 +20,15 @@ class Weather_map(tk.Tk):
         btn.place(x=x*100-1250, y=y*120-620)
         #btn.grid(column=int(x), row=int(y))
 
-    def display_map(self):
-        for i,city in enumerate(self.cities):
-            self.display_button(city)
 
+    def display_map(self):
+        w = weather_data.Weather()
+        for i,city in enumerate(self.cities):
+            print(city)
+            #weather = w.get_nearest_weather(city)
+            #print(weather)
+
+            self.display_button(city)
 
 
 if __name__ == '__main__':
