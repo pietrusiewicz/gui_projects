b0cd15b9eee70674d75cadee994c128f6d2ee9a4
pazdaninho <p4zdaninho.cr@gmail.com>
2023-09-22 08:05:14 +0200

    globalization calling function

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import os
 class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        self.func_dict = {'weather_map': lambda: [w := weather_map.Weather_map(), w.display_map_stations(), w.mainloop()]}
 
     def display_buttons(self):
-        func = lambda: [w := weather_map.Weather_map(), w.display_map_stations(), w.mainloop()]
-        btn = tk.Button(self, text="weather_app", command=func)
+        key = 'weather_map'
+        func = self.func_dict[key]
+        btn = tk.Button(self, text=key, command=func)
         #weather_map = importlib.import_module("weather_app.weather_map")
         #os.chdir("weather_app/weather_map")
         #btn.command = lambda: print("hell oworld")#
