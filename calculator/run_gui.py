5e24f6351be68c04acbc13894475bef7c973ead7
pazdaninho <p4zdaninho.cr@gmail.com>
2023-09-27 11:14:23 +0200

    globalize calculator

--- calculator/run_gui.py
+++ calculator/run_gui.py
@@ -1,21 +1,22 @@
 import tkinter as tk
 
-class Calculator(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
+class Calculator():
+    def __init__(self, master):
+        self.m = master
+        #tk.Tk.__init__(self)
         self.keyboard = [['7','8','9',  '+'],
                          ['6','5','4',  '-'],
                          ['3','2','1',  '*'],
                          ['0','00','C', '/']]
 
     def menu(self):
-        self.e = tk.Entry(self)
+        self.e = tk.Entry(self.m)
         self.e.grid(row=0, column=0, columnspan=4, sticky='W')
         #result = eval(f'{self.e.get()}')
         #tk.Label(self, text=f"= {result}").grid(row=0, column=0, columnspan=4, sticky='E')
         for i in range(4):
             for j in range(4):
-                b = tk.Button(self, text=f"{self.keyboard[i][j]}", width=3, height=2)
+                b = tk.Button(self.m, text=f"{self.keyboard[i][j]}", width=3, height=2)
                 b["command"] = lambda x = self.keyboard[i][j]: self.press_button(x)
                 #print(b.__dir__())
                 b.grid(row=i+1, column=j)
@@ -34,7 +35,7 @@ class Calculator(tk.Tk):
             #print(type(1)==int)
             if expression[-1] not in ['+', '-', '*', '/', '']:
                 result = eval(f'{expression}')
-                tk.Label(self, text=f"= {result}").grid(row=0, column=0, columnspan=4, sticky='E')
+                tk.Label(self.m, text=f"= {result}").grid(row=0, column=0, columnspan=4, sticky='E')
 
     def del_e(self):
         t = self.e.get()
@@ -44,9 +45,10 @@ class Calculator(tk.Tk):
     
     def get_result(self):
         result = eval(f"{self.e.get()}")
-        tk.Label(self, text=f"= {result}").grid(row=0, column=0, columnspan=4, sticky='E')
+        tk.Label(self.m, text=f"= {result}").grid(row=0, column=0, columnspan=4, sticky='E')
 
 if __name__ == '__main__':
-    c = Calculator()
+    r= tk.Tk()
+    c = Calculator(r)
     c.menu()
-    c.mainloop()
+    r.mainloop()
