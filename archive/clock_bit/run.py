91e075eed735c6e5e389c0841a8ebfee1de856bf
pazdaninho <p4zdaninho.cr@gmail.com>
2023-10-05 08:46:43 +0200

    convert inheritance to constructor

--- archive/clock_bit/run.py
+++ archive/clock_bit/run.py
@@ -3,9 +3,11 @@ from tkinter import Tk, Label
 from threading import Thread
 
 
-class App(Tk):
-    def __init__(self):
-        Tk.__init__(self)
+#class App(Tk):
+class App():
+    def __init__(self, master):
+        self.m = master
+        #Tk.__init__(self)
         self.display_bit_clock()
         t1 = Thread(target=self.work)
         t1.start()
@@ -15,7 +17,7 @@ class App(Tk):
         "display one or zero"
         # get current time
         s = time.strftime("%H:%M:%S")
-        Label(self, text=s).grid(row=0, column=0, columnspan=6)
+        Label(self.m, text=s).grid(row=0, column=0, columnspan=6)
         # get []
         s = [list(map(lambda x: bin(int(x.split()[0]))[2:], e.split()[0])) for e in s.split(":")]
 
@@ -25,16 +27,16 @@ class App(Tk):
             l += arr # }}}
 
         # place Labels with spaces
-        [[Label(self, text=" ",fg="#ececec", bg="#d2d2d2", bd=8).grid(row=y+1, column=x) for x in range(6)] for y in range(4)]
+        [[Label(self.m, text=" ",fg="#ececec", bg="#d2d2d2", bd=8).grid(row=y+1, column=x) for x in range(6)] for y in range(4)]
 
         # place one or zero value
         for i,bins in enumerate(l): # {{{
             # ['1','100']
             for j,num in enumerate(bins):
                 if int(num): 
-                    l = Label(self, text=num, bd=8, fg="white", bg="black")
+                    l = Label(self.m, text=num, bd=8, fg="white", bg="black")
                 else:
-                    l = Label(self, text=num, bd=8, fg="black", bg="white")
+                    l = Label(self.m, text=num, bd=8, fg="black", bg="white")
                 l.grid(row=j+1, column=i) # }}}
 
 
@@ -46,5 +48,6 @@ class App(Tk):
             self.display_bit_clock()
 
 if __name__ == '__main__':
-    a = App()
-    a.mainloop()
+    r = Tk()
+    a = App(r)
+    r.mainloop()
