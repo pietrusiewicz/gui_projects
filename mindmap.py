39842cf45d2b833ec99c4767da75d1f1bef6396b
pazdaninho <p4zdaninho.cr@gmail.com>
2023-10-09 09:21:03 +0200

    print self.tree

--- mindmap.py
+++ mindmap.py
@@ -1,24 +1,11 @@
-import tkinter as tk
-
 class MindMap:
-    def __init__(self, master):
-        self.m = master
-        self.m.geometry("1280x720")
-        self.tree = {}
-
-
-    def display_tree(self):
-        main_btn = tk.Button(self.m, text="glowna_mysl", command=self.enter_value)
-        main_btn.place(x=640, y=360)
+    def __init__(self):
+        self.tree = {'temat1':[], 'temat2':[]}
     
-
-    def enter_value(self):
-        e=tk.Entry(self.m)
-        e.place(x=640, y=360)
-
-
+    def display_tree(self):
+        for i,item in enumerate(self.tree):
+            print(item)
+    
 if __name__ == '__main__':
-    r = tk.Tk()
-    m = MindMap(r)
-    m.display_tree()
-    r.mainloop()
+    m = MindMap()
+
